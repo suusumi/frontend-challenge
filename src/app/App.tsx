00d4aea9c42f114e497ba3d36037acf12524ea6e
Sergey Groshev <wer113wer@yandex.ru>
2025-01-17 23:25:40 +0300

    [feat](routing): add HashRouter and write basic routing
    also move main.tsx (for FSD project structure)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,22 @@
-import '../App.css'
-import {Header} from "../widgets/header";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Header } from "../widgets/header";
+import { AllCatsPage } from "../pages/allCats";
+import { FavoriteCatsPage } from "../pages/favoriteCats";
+import {Box} from "@mui/material";
 
 function App() {
+    return (
+        <HashRouter>
+            <Header />
+            <Box sx={{marginTop:'80px'}}>
+                <Routes>
+                    <Route path="/" element={<AllCatsPage />} />
+                    <Route path="/favoriteCats" element={<FavoriteCatsPage />} />
+                </Routes>
+            </Box>
 
-
-  return (
-    <>
-      <Header/>
-    </>
-  )
+        </HashRouter>
+    );
 }
 
-export default App
+export default App;
