fc5d7e025c9b59dae786bb50e51436066d07cba1
Sergey Groshev <wer113wer@yandex.ru>
2025-01-17 23:46:27 +0300

    [feat](header): add routing in Header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import {Link} from "react-router-dom";
 
 export const Header: React.FC = () => {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -17,8 +18,8 @@ export const Header: React.FC = () => {
         <AppBar>
             <Toolbar>
                 <Box sx={{flexGrow: 1, display: {xs: "none", sm: "flex"}}}>
-                    <Button color="inherit">Все котики</Button>
-                    <Button color="inherit">Любимые котики</Button>
+                    <Button color="inherit" component={Link} to="/">Все котики</Button>
+                    <Button color="inherit" component={Link} to="/favoriteCats">Любимые котики</Button>
                 </Box>
                 <Box sx={{display: {xs: "flex", sm: "none"}}}>
                     <IconButton
@@ -29,8 +30,8 @@ export const Header: React.FC = () => {
                         <MenuIcon/>
                     </IconButton>
                     <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-                        <MenuItem>Все котики</MenuItem>
-                        <MenuItem>Любимые котики</MenuItem>
+                        <MenuItem onClick={handleMenuClose} component={Link} to="/">Все котики</MenuItem>
+                        <MenuItem onClick={handleMenuClose} component={Link} to="/favoriteCats">Любимые котики</MenuItem>
                     </Menu>
                 </Box>
             </Toolbar>
