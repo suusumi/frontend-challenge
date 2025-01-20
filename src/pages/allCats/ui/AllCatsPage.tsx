dc9de1e4166b69bc092280473f38be5d2727393a
Sergey Groshev <wer113wer@yandex.ru>
2025-01-20 18:47:41 +0300

    [feat](all cats page): limitless scrolling

--- src/pages/allCats/ui/AllCatsPage.tsx
+++ src/pages/allCats/ui/AllCatsPage.tsx
@@ -1,7 +1,7 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import {fetchCats} from "../../../entities/cat/api/fetchCats";
-import {Box, Button, CircularProgress,} from "@mui/material";
-import Grid from '@mui/material/Grid2';
+import {Box, CircularProgress, Typography} from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import {Cat} from "../../../entities/cat/model/types";
 import {CatCard} from "../../../entities/cat/ui/CatCard";
 import {getFavorites, saveFavorites, toggleFavorites} from "../../../entities/cat/model/favorites.ts";
@@ -12,12 +12,13 @@ export const AllCatsPage: React.FC = () => {
     const [page, setPage] = useState(0);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
+    const observerRef = useRef<HTMLDivElement | null>(null);
 
     const loadCats = useCallback(async (currentPage: number) => {
         try {
             setLoading(true);
             const newCats = await fetchCats(currentPage);
-            setCats(newCats);
+            setCats((prevCats) => [...prevCats, ...newCats]);
             setHasMore(newCats.length > 0);
         } catch (error) {
             console.error(error);
@@ -40,51 +41,63 @@ export const AllCatsPage: React.FC = () => {
         saveFavorites(updatedFavorites);
     };
 
-    const handleNextPage = () => {
-        setPage((prevPage) => prevPage + 1);
-    };
+    useEffect(() => {
+        if (!hasMore || loading) return;
 
-    const handlePreviousPage = () => {
-        if (page > 0) setPage((prevPage) => prevPage - 1);
-    };
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    setPage((prevPage) => {
+                        return loading ? prevPage : prevPage + 1;
+                    });
+                }
+            },
+            {threshold: 1.0}
+        );
+
+        if (observerRef.current) {
+            observer.observe(observerRef.current);
+        }
+
+        return () => {
+            if (observerRef.current) observer.unobserve(observerRef.current);
+        };
+    }, [hasMore, loading]);
+
 
     return (
         <Box>
-            {loading ? (
-                <CircularProgress sx={{display: "block", margin: "20px auto"}}/>
-            ) : (
-                <Grid container spacing={2}>
-                    {cats.map((cat) => (
-                        <Grid size={{xs: 12, sm: 4, md: 2}}>
-                            <CatCard
-                                cat={cat}
-                                isFavorite={favorites.some((fav) => fav.id === cat.id)}
-                                onToggleFavorite={() => handleToggleFavorite(cat)}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
+            {!loading && cats.length === 0 && (
+                <Box sx={{textAlign: "center", marginTop: "20px"}}>
+                    <Typography>Котики не найдены / Ошибка загрузки данных</Typography>
+                </Box>
             )}
 
-            <Box sx={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handlePreviousPage}
-                    disabled={page === 0 || loading}
-                    sx={{marginRight: "10px"}}
-                >
-                    Назад
-                </Button>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleNextPage}
-                    disabled={!hasMore || loading}
-                >
-                    Вперёд
-                </Button>
-            </Box>
+            <Grid container spacing={2}>
+                {cats.map((cat) => (
+                    <Grid key={cat.id} size={{xs: 12, sm: 4, md: 2}}>
+                        <CatCard
+                            cat={cat}
+                            isFavorite={favorites.some((fav) => fav.id === cat.id)}
+                            onToggleFavorite={() => handleToggleFavorite(cat)}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+
+            {loading && (
+                <Box sx={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
+                    <CircularProgress/>
+                </Box>
+            )}
+
+            {!hasMore && !loading && (
+                <Box sx={{textAlign: "center", marginTop: "20px"}}>
+                    <Typography>Котики закончились</Typography>
+                </Box>
+            )}
+
+            <div ref={observerRef} style={{height: "1px"}}/>
         </Box>
     );
 };
